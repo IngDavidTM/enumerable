cc05c7cd5efe5b4d45aaab520e0688fa2a69349c
IngDavidTM <david-1011@hotmail.com>
2022-12-15 11:36:57 -0500

    add module

---
+++ myEnumerable.rb
@@ -0,0 +1,23 @@
+module MyEnumerable
+  def all?
+    @list.each do |word|
+      return false unless yield word
+    end
+    true
+  end
+
+  def any?
+    @list.each do |word|
+      return true if yield word
+    end
+    false
+  end
+
+  def filter
+    filtered_list = []
+    @list.each do |e|
+      filtered_list << e if yield e
+    end
+    filtered_list
+  end
+end
